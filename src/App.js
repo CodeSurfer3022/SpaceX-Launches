f32cfbea99bbea35c2f0b93e27f08abdc83e3969
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-17 23:41:51 +0530

    Add basic grid styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import List from './List';
 function App() {
   return (
     <div className="App">
-      SpaceX Launches
-      <Filters />
-      <List />
+        <header>
+            SpaceX Launches
+        </header>
+        <div className="main">
+            <Filters />
+            <List />
+        </div>
+        <footer>
+            Developed by Chandra.
+        </footer>
     </div>
   );
 }
