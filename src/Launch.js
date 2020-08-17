9973652a0301cf3fd9ff15fa71163d0fb16968e7
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-17 22:29:02 +0530

    Put info in launch cards in the list

--- src/Launch.js
+++ src/Launch.js
@@ -1,44 +1,28 @@
 import React from 'react';
 
-let url = "https://api.spacexdata.com/v3/launches?limit=100";
-
-function getInfo(response) {
-    return {
-        mission_name: response.mission_name,
-        flight_number: response.flight_number,
-        mission_ids: response.mission_id,
-        launch_year: response.launch_year,
-        launch_success: response.launch_success,
-        launch_landing: response.launch_landing
-    }
-}
-
-class Launch extends React.Component{
-    constructor(props) {
-        super(props);
-        this.getLaunches().then( response => {
-            let missions = [];
-                for(let mission of response) {
-                    missions.push(getInfo(mission));
-                }
-                console.log(missions);
-            }
-        );
-    }
-
-    async getLaunches() {
-        const data = await fetch(url, {mode: 'cors'});
-        const response = await data.json();
-        console.log(response);
-        return response;
-    }
-    render() {
-        return(
+function Launch(props) {
+    return(
+        <div>
+            <br />
+            {console.log(props.value)}
             <div>
-
+                {props.value.mission_name}#{props.value.flight_number}
             </div>
-        )
-    }
+            <div>
+                {props.value.mission_ids}
+            </div>
+            <div>
+                {props.value.launch_year}
+            </div>
+            <div>
+                {props.value.launch_success ? "true" : "false"}
+            </div>
+            <div>
+                {props.value.launch_landing}
+            </div>
+            <br />
+        </div>
+    )
 }
 
 export default Launch;
--- src/List.js
+++ src/List.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import Launch from './Launch';
 
-class List extends React.Component{
+let url = "https://api.spacexdata.com/v3/launches?limit=100";
 
-    render(){
+function getInfo(response) {
+    return {
+        mission_name: response.mission_name,
+        flight_number: response.flight_number,
+        mission_ids: response.mission_id,
+        launch_year: response.launch_year,
+        launch_success: response.launch_success,
+        launch_landing: response.launch_landing
+    }
+}
+
+class List extends React.Component{
+    constructor(props) {
+        super(props);
+        this.getLaunches().then( launches => {
+                let missions = [], missionInfo, mission;
+                for(let launch of launches) {
+                    missionInfo = getInfo(launch);
+                    mission = <Launch key={missionInfo.mission_name}
+                                      value={missionInfo} />;
+                    missions.push(mission)
+                }
+                console.log(missions);
+                this.setState({missions: missions});
+            }
+        );
+    }
+
+    async getLaunches() {
+        const data = await fetch(url, {mode: 'cors'});
+        return await data.json();
+    }
+    render() {
+        if(!this.state) {
+            return(
+                <div>
+                    wait
+                </div>
+            )
+        }
         return(
             <div>
-                <Launch />
+                {this.state.missions}
             </div>
         )
     }
-
 }
 
 export default List;
