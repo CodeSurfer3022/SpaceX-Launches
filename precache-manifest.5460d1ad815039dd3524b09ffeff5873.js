a6454a84cf82045ebc6c4b30dfec46e764d88adb
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-10-25 12:14:17 +0530

    Updates

---
+++ precache-manifest.5460d1ad815039dd3524b09ffeff5873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1773bde50d9ef3c51e898184e9daee",
+    "url": "/SpaceX-Launches/index.html"
+  },
+  {
+    "revision": "c29f62a5b2a7086919c0",
+    "url": "/SpaceX-Launches/static/js/2.6d0a725d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpaceX-Launches/static/js/2.6d0a725d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b413334a7c5813a0fd",
+    "url": "/SpaceX-Launches/static/js/main.12366ac3.chunk.js"
+  },
+  {
+    "revision": "7d944790ef7f6dd57a8f",
+    "url": "/SpaceX-Launches/static/js/runtime-main.97d54f0d.js"
+  }
+]);
